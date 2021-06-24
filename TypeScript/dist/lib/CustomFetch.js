2035a665d25ec57f87b651d20ed80afcc84b06c9
Cole McMannus <colemcmannus@gmail.coms>
2021-06-24 10:19:25 -0400

    proxy apollo requests based on env variables

--- TypeScript/dist/lib/CustomFetch.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=CustomFetch.js.map
--- TypeScript/src/test.ts
+++ TypeScript/src/test.ts
@@ -1,7 +1,45 @@
 import gql from 'graphql-tag';
 import { CreateApolloClient } from '.';
 import { UserAuthTicket } from './lib/AuthClient';
-
+const addToCurrentCartQuery = gql`
+mutation addToCart($productToAdd:CartItem_Input!){
+    addItemToCurrentCart(cartItem_Input: $productToAdd) {
+      id
+    }
+}`
+const currentCartQuery = gql`
+query GetCurrentCart {
+    cart: currentCart {
+      id
+      total
+      userId
+      items {
+        id
+        total
+        product {
+          productCode
+          name
+          imageUrl
+        }
+      }
+    }
+  }
+`
+function buildAddToCartVariables(productCode="", quantity=1) {
+  return {
+      productToAdd: { 
+        product:
+            {
+                isTaxable: true,
+                isRecurring: false,
+                productCode: productCode,
+                isPackagedStandAlone: true
+            },
+            quantity:quantity,
+            fulfillmentMethod:"Ship"
+        }
+    }
+}
 (async function() {
   let rawTicket: string = "";
   let ticket: UserAuthTicket | undefined = undefined;
@@ -12,32 +50,33 @@ import { UserAuthTicket } from './lib/AuthClient';
       }
     },
     onTicketRead: () => {
+      if(!ticket){
+        console.log('no ticket')
+      }
       return ticket as UserAuthTicket;
     },
     onTicketRemove: () => {
       ticket = undefined;
     }
   }
-
+  const at_url = "https://home.dev07.kubedev.kibo-dev.com/api/platform/applications/authtickets/oauth"
   const cfg = {
-    "accessTokenUrl": "https://home.mozu.com/api/platform/applications/authtickets/oauth",
-    "clientId": "d4e9bb5.cmcmannus.1.0.0.Release",
-    "sharedSecret": "20feabf08fb14e0b86fb5335f98661ce",
-    "apiHost": "https://t30295-s50528.sandbox.mozu.com"
+    "accessTokenUrl": at_url,
+    "clientId": "mozuadmin.mpoc_app.1.0.0.Release",
+    "sharedSecret": "1d16d7d35b8542cba2f99216f20dca32",
+    "apiHost": "https://t17194-s21127.dev07.kubedev.kibo-dev.com/"
   }
-
-
   try{
     const apolloClient = CreateApolloClient({
       api: cfg,
       clientAuthHooks
     });
-
     const results = await apolloClient.query({
       query: gql`
         query GetProducts {
           products {
             items {
+              productCode
               content {
                 productName
               }
@@ -46,8 +85,15 @@ import { UserAuthTicket } from './lib/AuthClient';
         }
       `
     })
-
-    console.log(results?.data?.products?.items);
+    const productCode = results?.data?.products?.items[0]?.productCode
+    const addToCartResp = await apolloClient.mutate({
+      mutation: addToCurrentCartQuery,
+      variables: buildAddToCartVariables(productCode)
+    })
+    const currentCart = await apolloClient.query({
+      query: currentCartQuery
+    })
+    console.log(currentCart)
   } catch (ex) {
     console.log(ex);
   }
