e0b82da5ce743871ec2b0b7f6b48e7a68e97a535
Chandradeepta <43542673+Chandradeepta@users.noreply.github.com>
2021-10-20 17:45:05 +0530

    refactored async functions

--- src/lib/TicketManager.ts
+++ src/lib/TicketManager.ts
@@ -1,121 +1,143 @@
 import { UserAuthTicket } from './AuthClient'
-import AuthClient from './AuthClient';
+import AuthClient from './AuthClient'
 
 export interface TicketStorageManager {
-  ticketFetcher: (client: AuthClient) => Promise<UserAuthTicket>;
-  onTicketChanged: (ticket: UserAuthTicket) => void;
-  onTicketRefreshed: (ticket: UserAuthTicket) => void;
-  onTicketRemoved: () => void;
+  ticketFetcher: (client: AuthClient) => Promise<UserAuthTicket>
+  onTicketChanged: (ticket: UserAuthTicket) => void
+  onTicketRefreshed: (ticket: UserAuthTicket) => void
+  onTicketRemoved: () => void
 }
 
 export interface TicketManagerOptions {
-  authClient: AuthClient;
-  storageManager: TicketStorageManager;
-  ticket?: UserAuthTicket;
+  authClient: AuthClient
+  storageManager: TicketStorageManager
+  ticket?: UserAuthTicket
 }
 
 const formatTicket: (auth: UserAuthTicket) => UserAuthTicket = (auth) => {
-  auth.accessTokenExpiration = new Date(auth.accessTokenExpiration);
-  auth.refreshTokenExpiration = new Date(auth.refreshTokenExpiration);
-  delete auth.jwtAccessToken;
-  return auth;
+  auth.accessTokenExpiration = new Date(auth.accessTokenExpiration)
+  auth.refreshTokenExpiration = new Date(auth.refreshTokenExpiration)
+  delete auth.jwtAccessToken
+  return auth
 }
 
 export default class TicketManager {
   /// Private Fields
-  private _authClient: AuthClient;
-  private _authTicket: UserAuthTicket | undefined;
-  private _storageManager: TicketStorageManager;
-  private _ticketFetcherPromise: Promise<UserAuthTicket> | undefined;
-  private _ticketRefreshPromise: Promise<UserAuthTicket> | undefined;
+  private _authClient: AuthClient
+  private _authTicket: UserAuthTicket | undefined
+  private _storageManager: TicketStorageManager
+  private _ticketFetcherPromise: UserAuthTicket | undefined
+  private _ticketRefreshPromise: UserAuthTicket | undefined
 
   constructor(options: TicketManagerOptions) {
-    this._authClient = options.authClient;
-    this._storageManager = options.storageManager;
-    if (options.ticket) this._authTicket = formatTicket(options.ticket);
+    this._authClient = options.authClient
+    this._storageManager = options.storageManager
+    if (options.ticket) this._authTicket = formatTicket(options.ticket)
   }
 
   /// Private Methods
-  private _validateTicket: (authTicket: UserAuthTicket) => void = (authTicket) => {
-    if (!authTicket.accessToken ||
-        !authTicket.refreshToken ||
-        !authTicket.jwtAccessToken) throw new Error('Invalid auth ticket!');
+  private _validateTicket: (authTicket: UserAuthTicket) => void = (
+    authTicket,
+  ) => {
+    if (
+      !authTicket.accessToken ||
+      !authTicket.refreshToken ||
+      !authTicket.jwtAccessToken
+    )
+      throw new Error('Invalid auth ticket!')
   }
 
   private _performTicketFetch: () => Promise<UserAuthTicket> = async () => {
-    if (!this._storageManager?.ticketFetcher) return Promise.reject('No implementation for ticketFetcher provided.');
-    if (!this._authClient) return Promise.reject('No implementation for AuthClient provided.');
-
-    if (this._ticketFetcherPromise) return this._ticketFetcherPromise;
-    this._ticketFetcherPromise = this._storageManager.ticketFetcher(this._authClient);
-    return this._ticketFetcherPromise.then(resp => {
-      this._ticketFetcherPromise = undefined;
-      return resp;
-    }).catch(err => {
-      this._ticketFetcherPromise = undefined;
-      throw err;
-    })
-  }
-
-  private _performTicketRefresh: (authTicket: UserAuthTicket) => Promise<UserAuthTicket> = async (authTicket) => {
-    if (!this._authClient) return Promise.reject('No implementation for AuthClient provided.');
-
-    if (this._ticketRefreshPromise) return this._ticketRefreshPromise;
-    this._ticketRefreshPromise = this._authClient.refreshUserAuth(authTicket);
-    return this._ticketRefreshPromise.catch(err => {
-      throw err;
-    }).finally(() => {
-      this._ticketRefreshPromise = undefined;
-    })
-  }
-
-  private _refreshTicket: (authTicket?: UserAuthTicket) => Promise<UserAuthTicket> = async authTicket => {
-    if (!this._authClient) return Promise.reject('No implementation for AuthClient provided.');
-
-    let newTicketPromise;
-
-    if (authTicket !== null && authTicket !== undefined) {
-      if (authTicket.refreshToken && new Date() < authTicket.refreshTokenExpiration) {
-        newTicketPromise = this._performTicketRefresh(authTicket);
-      } else {
-        newTicketPromise = this._performTicketFetch();
-      }
-    } else {
-      newTicketPromise = this._performTicketFetch();
+    if (this._ticketFetcherPromise) return this._ticketFetcherPromise
+    try {
+      this._ticketFetcherPromise = await this._storageManager.ticketFetcher(
+        this._authClient,
+      )
+      return this._ticketFetcherPromise
+    } catch (err) {
+      this._ticketFetcherPromise = undefined
+      throw err
     }
-      
-    return newTicketPromise.then(ticket => {
-      if (this._storageManager?.onTicketRefreshed) this._storageManager.onTicketRefreshed(ticket);      
-      return this.setTicket(ticket);
-    });
+  }
+
+  private _performTicketRefresh: (
+    authTicket: UserAuthTicket,
+  ) => Promise<UserAuthTicket> = async (authTicket) => {
+    if (this._ticketRefreshPromise) return this._ticketRefreshPromise
+    try {
+      this._ticketRefreshPromise = await this._authClient.refreshUserAuth(
+        authTicket,
+      )
+      return this._ticketRefreshPromise
+    } catch (err) {
+      throw err
+    } finally {
+      this._ticketRefreshPromise = undefined
+    }
+  }
+
+  private _refreshTicket: (
+    authTicket?: UserAuthTicket,
+  ) => Promise<UserAuthTicket> = async (authTicket) => {
+    let newTicketPromise: UserAuthTicket
+    try {
+      if (authTicket !== null && authTicket !== undefined) {
+        if (
+          authTicket.refreshToken &&
+          new Date() < authTicket.refreshTokenExpiration
+        ) {
+          newTicketPromise = await this._performTicketRefresh(authTicket)
+        } else {
+          newTicketPromise = await this._performTicketFetch()
+        }
+      } else {
+        newTicketPromise = await this._performTicketFetch()
+      }
+      if (this._storageManager?.onTicketRefreshed)
+        this._storageManager.onTicketRefreshed(newTicketPromise)
+      return this.setTicket(newTicketPromise)
+    } catch (err) {
+      throw err
+    }
   }
 
   /// Public Methods
-  setTicket: (authTicket: UserAuthTicket) => UserAuthTicket = authTicket => {
-    this._validateTicket(authTicket);
-    this._authTicket = formatTicket(authTicket);
-    this._storageManager?.onTicketChanged?.(authTicket);
-    return authTicket;
+  setTicket: (authTicket: UserAuthTicket) => UserAuthTicket = (authTicket) => {
+    this._validateTicket(authTicket)
+    this._authTicket = formatTicket(authTicket)
+    this._storageManager?.onTicketChanged?.(authTicket)
+    return authTicket
   }
 
   getTicket: () => Promise<UserAuthTicket> = async () => {
-    if (this._authTicket && !TicketManager.isTicketExpired(this._authTicket)) return Promise.resolve(this._authTicket);
-    return this._refreshTicket(this._authTicket);
+    if (this._authTicket && !TicketManager.isTicketExpired(this._authTicket))
+      return Promise.resolve(this._authTicket)
+    return this._refreshTicket(this._authTicket)
   }
 
   getToken: () => Promise<string> = async () => {
-    return this.getTicket().then(ticket => {
-      return ticket.accessToken || '';
-    })
+    try {
+      const ticket = await this.getTicket()
+      return ticket.accessToken || ''
+    } catch (err) {
+      throw err
+    }
   }
 
   invalidateTicket: () => void = () => {
-    this._authTicket = undefined;
-    this._storageManager.onTicketRemoved();
+    this._authTicket = undefined
+    this._storageManager.onTicketRemoved()
   }
 
   /// Static Methods
-  static isTicketExpired: (authTicket: UserAuthTicket) => boolean = (authTicket) => {
-    return !authTicket || !authTicket.accessToken || !authTicket.accessTokenExpiration || new Date() > authTicket.accessTokenExpiration;
+  static isTicketExpired: (authTicket: UserAuthTicket) => boolean = (
+    authTicket,
+  ) => {
+    return (
+      !authTicket ||
+      !authTicket.accessToken ||
+      !authTicket.accessTokenExpiration ||
+      new Date() > authTicket.accessTokenExpiration
+    )
   }
 }
